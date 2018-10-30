d662f0e62ca80b4c95a1c556d781f55e2cf7433f
grphine <grphine@gmail.com>
2018-10-30 20:36:53 +0000

    Initial Commit

---
+++ timetable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timetable
+//
+//  Created by Juheb on 30/10/2018.
+//  Copyright © 2018 Juheb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
