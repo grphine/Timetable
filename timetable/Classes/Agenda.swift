6369ae40b27b75a3d2ed507df5bb95664d731777
grphine <grphine@gmail.com>
2018-11-10 19:13:50 +0000

    fix black status menu

--- timetable/Classes/Agenda.swift
+++ timetable/Classes/Agenda.swift
@@ -31,11 +31,14 @@ class AgendaViewController: UIViewController {
         // Define the menus
         SideMenuManager.default.menuLeftNavigationController = storyboard!.instantiateViewController(withIdentifier: "LeftMenuNavigationController") as? UISideMenuNavigationController
         //SideMenuManager.default.menuRightNavigationController = storyboard!.instantiateViewController(withIdentifier: "RightMenuNavigationController") as? UISideMenuNavigationController
+
         
         // Enable gestures. The left and/or right menus must be set up above for these to work.
         // Note that these continue to work on the Navigation Controller independent of the View Controller it displays!
         SideMenuManager.default.menuAddPanGestureToPresent(toView: self.navigationController!.navigationBar)
         SideMenuManager.default.menuAddScreenEdgePanGesturesToPresent(toView: self.navigationController!.view)
+        
+        SideMenuManager.default.menuFadeStatusBar = false
         
     }
     
