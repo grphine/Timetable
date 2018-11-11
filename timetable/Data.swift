c6568c061bdeb30a5aba9f4b086356a5bbc85990
grphine <grphine@gmail.com>
2018-11-11 14:06:25 +0000

    populated some data

--- timetable/Data.swift
+++ timetable/Data.swift
@@ -5,4 +5,26 @@
 //  Created by Juheb on 11/11/2018.
 //
 
+public var Data = ["Maths": [0x778BCD, ["M": [9, 12, 13], "Tu": [9]], "Maths lessons", "reminders"], "English": [0xF2F16F, ["M": [10, 14, 15], "Tu": [10]], "English lessons", "reminders"]]
 
+
+
+/* array of dictionaries, for each item
+this is for recurring items
+ 
+ [  {subject/name: [hex colour, [{day: [times]}], description, reminders] } ]
+ 
+ */
+
+
+/*
+ 
+ let dateString = "25/01/2011"
+ 
+ let dateFormatter = DateFormatter()
+ 
+ dateFormatter.dateFormat = "dd/MM/yyyy"
+ 
+ let dateFromString = dateFormatter.date(from: dateString)
+ 
+ */
