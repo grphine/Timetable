53eb6de0b3f68a2fccf67eb679db8157f74f201c
grphine <grphine@gmail.com>
2018-11-11 14:19:24 +0000

    created repeating and single events

--- timetable/Data.swift
+++ timetable/Data.swift
@@ -5,7 +5,9 @@
 //  Created by Juheb on 11/11/2018.
 //
 
-public var Data = ["Maths": [0x778BCD, ["M": [9, 12, 13], "Tu": [9]], "Maths lessons", "reminders"], "English": [0xF2F16F, ["M": [10, 14, 15], "Tu": [10]], "English lessons", "reminders"]]
+public var RepeatingEvents = ["Maths": [0x778BCD, ["M": [9, 12, 13], "Tu": [9]], "Maths lessons", "reminders"], "English": [0xF2F16F, ["M": [10, 14, 15], "Tu": [10]], "English lessons", "reminders"]]
+
+public var SingleEvents = ["dd/MM/yyyy": ["subject/name", "time", "colour", "description", "reminders"]]
 
 
 
