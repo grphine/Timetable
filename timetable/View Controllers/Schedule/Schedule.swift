f51fd3cdc17e2ebe502f6dbce7909ef6c46cf563
grphine <grphine@gmail.com>
2018-11-11 18:55:55 +0000

    attempt to fix events not appearing

--- timetable/View Controllers/Schedule/Schedule.swift
+++ timetable/View Controllers/Schedule/Schedule.swift
@@ -24,7 +24,7 @@ class ScheduleView: UIViewController, SpreadsheetViewDataSource, SpreadsheetView
                      UIColor(red: 0.400, green: 0.584, blue: 0.141, alpha: 1),
                      UIColor(red: 0.835, green: 0.655, blue: 0.051, alpha: 1),
                      UIColor(red: 0.153, green: 0.569, blue: 0.835, alpha: 1)]
-    let hours = ["6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 AM", "1:00 PM", "2:00 PM",
+    let hours = ["6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM",
                  "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM", "9:00 PM", "10:00 PM", "11:00 PM"]
     let evenRowColor = UIColor(red: 0.914, green: 0.914, blue: 0.906, alpha: 1)
     let oddRowColor: UIColor = .white
@@ -56,8 +56,8 @@ class ScheduleView: UIViewController, SpreadsheetViewDataSource, SpreadsheetView
         spreadsheetView.register(ScheduleCell.self, forCellWithReuseIdentifier: String(describing: ScheduleCell.self))
         
        
-        let english = Event(name: "English", colour: UIColor(red: 0.918, green: 0.224, blue: 0.153, alpha: 1), occurences: [[10,11],[10],[2],[4],[1],[],[]], description: "english lesson")
-        let maths = Event(name: "Maths", colour: UIColor(red: 0.200, green: 0.620, blue: 0.565, alpha: 1), occurences: [[9, 12, 13],[9],[1],[3],[4,12,5,4],[],[]], description: "maths lesson")
+        let english = Event(name: "English", colour: UIColor(red: 0.918, green: 0.224, blue: 0.153, alpha: 1), occurences: [[],[],[],[],[],[],[]], description: "english lesson")
+        let maths = Event(name: "Maths", colour: UIColor(red: 0.200, green: 0.620, blue: 0.565, alpha: 1), occurences: [[6],[7,8],[9,10,11],[12,13,14,15],[],[14],[14]], description: "maths lesson")
         
         RepeatingEvents.append(english)
         RepeatingEvents.append(maths)
@@ -109,8 +109,10 @@ class ScheduleView: UIViewController, SpreadsheetViewDataSource, SpreadsheetView
         var subject = ""
         
         for event in RepeatingEvents{
-            if event.occurences[column-1] == [row-4]{
+            for rows in event.occurences[column-1]{
+                if rows == row + 4{
                 subject = event.name
+                }
             } //make clause for multiple items / ensure no conflicts
         }
         return subject
