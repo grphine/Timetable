f45acc680b440753ee33b03dc9871981c1078d67
grphine <grphine@gmail.com>
2018-11-15 18:53:47 +0000

    events and their data can now be pulled using event data class

--- timetable/Classes/RealmSetupFile.swift
+++ timetable/Classes/RealmSetupFile.swift
@@ -36,6 +36,77 @@ class Event: Object{
     
 }
 
+class EventData: Event{
+    
+    //MARK: nameByCell
+    func nameByCell(array: [Event], column: Int, row: Int) -> String{ //get event name by cell
+            var subject = ""
+            for event in array{
+                for rows in event.occurences[column-1]{
+                    if rows == row + 4{
+                        subject = event.name
+                    }
+                }
+            }
+            return subject
+    
+        }
+    
+    //MARK: colourByCell
+    func colourByCell(array: [Event], column: Int, row: Int) -> UIColor{ //get event colour by cell
+        var colour = UIColor()
+        for event in array{
+            for rows in event.occurences[column-1]{
+                if rows == row + 4{
+                    colour = event.colour
+                }
+            }
+        }
+        return colour
+
+    }
+
+    //MARK: descByCell
+    func descByCell(array: [Event], column: Int, row: Int) -> String{ //get event description by cell
+        var desc = ""
+        for event in array{
+            for rows in event.occurences[column-1]{
+                if rows == row + 4{
+                    desc = event.description
+                }
+            }
+        }
+        return desc
+
+    }
+
+    //MARK: eventByCell
+    func eventByCell(array: [Event], column: Int, row: Int) -> Event{ //get event by cell
+        var event = Event()
+        for item in array{
+            for rows in item.occurences[column-1]{
+                if rows == row + 4{
+                    event = item
+                }
+            }
+        }
+        return event
+    }
+
+    //MARK: eventByName
+    func eventByName(array: [Event], name: String) -> Event { //get event by event name
+        var event = Event()
+        for item in array{
+            if item.name == name{
+                event = item
+            }
+        }
+        return event
+    }
+    
+    
+}
+
 
 
 extension Results {
