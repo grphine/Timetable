b297085f6b93ed577535d0b9d6d1392a940602e2
grphine <grphine@gmail.com>
2018-11-28 14:25:56 +0000

    completed implementation

--- collapsible tableview/TableViewController.swift
+++ collapsible tableview/TableViewController.swift
@@ -9,14 +9,80 @@
 import UIKit
 
 class CollapsibleTableViewController: UITableViewController {
+    
+    var sections = sectionsData
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.estimatedRowHeight = 44.0
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        self.tableView.allowsMultipleSelection = true
+        self.tableView.allowsMultipleSelectionDuringEditing = true
+        
+        self.title = "Select Times"
 
     }
 
 }
 
 extension CollapsibleTableViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].collapsed ? 0 : sections[section].items.count
+    }
+    
+    //Cell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: CollapsibleTableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell") as? CollapsibleTableViewCell ??
+            CollapsibleTableViewCell(style: .default, reuseIdentifier: "cell")
+        
+        let item = sections[indexPath.section].items[indexPath.row]
+        
+        cell.nameLabel.text = String(describing: item)
+        
+        return cell
+    }
+    
+    // Header
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "header") as? CollapsibleTableViewHeader ?? CollapsibleTableViewHeader(reuseIdentifier: "header")
+        
+        header.titleLabel.text = sections[section].name
+        header.arrowLabel.text = ">"
+        
+        header.setCollapsed(sections[section].collapsed)
+        
+        header.section = section
+        header.delegate = self
+        
+        return header
+    }
+    
+    
+}
+
+// MARK: - Section Header Delegate
+//
+extension CollapsibleTableViewController: CollapsibleTableViewHeaderDelegate {
+    
+    func toggleSection(_ header: CollapsibleTableViewHeader, section: Int) {
+        
+        print("toggleSection")
+        let collapsed = !sections[section].collapsed
+        
+        // Toggle collapse
+        sections[section].collapsed = collapsed
+        header.setCollapsed(collapsed)
+        
+        //
+        
+        tableView.reloadSections(NSIndexSet(index: section) as IndexSet, with: .automatic)
+    }
     
 }
